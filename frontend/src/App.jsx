a8966558e9196f487b5ea4aa47e559cf3c5fa2e6
Surya Pratap Singh <sp7763360@gmail.com>
2024-10-26 12:36:50 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Create from './components/Create'
 import Read from './components/Read'
 import Update from './components/Update'
 
-function App() {
+function App() { 
   const [count, setCount] = useState(0)
   return (
     <>
